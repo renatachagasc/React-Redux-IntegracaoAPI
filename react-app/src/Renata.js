6d561188b3076bb5c8625d7156dc19bac9c8cff8
renatachagasc <rehcardosochagas@gmail.com>
2019-06-11 11:48:19 -0300

    componentes e propriedades

--- react-app/src/Renata.js
+++ react-app/src/Renata.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import User from './User';
+import photo from './img/user.png'
+
 
 class Renata extends Component {
     render() {
         return (
             <div>
                 Eu sou um componente chamando Renata
+                <User name= "Renata" photo={photo}></User>
+                <User name= "Cris"></User>
+
             </div>
         );
     }
