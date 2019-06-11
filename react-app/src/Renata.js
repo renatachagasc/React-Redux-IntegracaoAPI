1d8fff09915e215fa157cf99cdfe00df5a78ac0c
renatachagasc <rehcardosochagas@gmail.com>
2019-06-11 11:30:26 -0300

    primeiro component

---
+++ react-app/src/Renata.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Renata extends Component {
+    render() {
+        return (
+            <div>
+                Eu sou um componente chamando Renata
+            </div>
+        );
+    }
+}
+
+export default Renata;
